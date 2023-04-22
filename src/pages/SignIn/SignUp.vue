b394c94279bbe7f005cb8014829f10c7ae6f25e0
sky04281 <sky04282@gmail.com>
2023-04-22 16:02:21 +0800

    sign up

---
+++ src/pages/SignIn/SignUp.vue
@@ -0,0 +1,43 @@
+<script setup>
+    //firebase 
+    import { auth } from "@/scripts/firebase/config.js";
+    import { createUserWithEmailAndPassword } from "firebase/auth";
+
+    import { ref } from "vue";
+    //component
+    import MaterialInput from "@/components/MaterialInput.vue";
+
+    const email = ref("");
+    const password = ref("");
+
+    function signUp(){
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                window.location = "/";
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
+</script>
+
+<template>
+    <MaterialInput
+        class="input-group-outline"
+        :label="{ text: '信箱', class: 'form-label' }"
+        type="text"
+        v-model.lazy="email"
+    />
+    <br>
+    <MaterialInput
+        class="input-group-outline"
+        :label="{ text: '密碼', class: 'form-label' }"
+        type="text"
+        v-model.lazy="password"
+    />
+    <br>
+    <input type="button" value="註冊" @click="signUp">
+
+</template>
