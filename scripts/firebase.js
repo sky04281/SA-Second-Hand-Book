76fb4d321f0efb3fd41bf7da15f6f7d2ab6e1aee
sky04281 <sky04282@gmail.com>
2023-04-23 18:00:13 +0800

    del node
    npm 用了有 bug 不用了
    改用 cdn

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js"
 
 const firebaseConfig = {
     apiKey: "AIzaSyB44Av5SScdqcEMoWlndNIOU7AZBEtiO20",
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+export { app, auth, db };
