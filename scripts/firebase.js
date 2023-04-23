f69489c8a9c25235897a1802e1c5028ab4eb6cf0
sky04281 <sky04282@gmail.com>
2023-04-23 18:29:16 +0800

    write-test
    可以寫入

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyB44Av5SScdqcEMoWlndNIOU7AZBEtiO20",
