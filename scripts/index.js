6727f9574c3b7ca4d624628bb449c5490a536d59
sky04281 <sky04282@gmail.com>
2023-04-23 22:43:54 +0800

    logout

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,24 @@
 import { auth } from "../scripts/firebase.js";
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
-        
+
+const navbar = document.getElementById("nav-account");
+
+
 onAuthStateChanged(auth, (user) => {
+    if(user){
     console.log(user);
+        navbar.innerHTML = "<a href='#' class='nav-item nav-link' id='btn-logout'>登出</a>";
+        
+        const btn = document.getElementById("btn-logout");
+        btn.addEventListener("click", (e) => {
+            e.preventDefault;
+            signOut(auth);
+            alert("登出成功!");
+        });
+    }else{
+        navbar.innerHTML = "<a href='login.html' class='nav-item nav-link'>登入</a>"
+        +"<a href='register.html' class='nav-item nav-link'>註冊</a>";
+    }
 });
 
-// const logOutBtn = document.getElementById("btn-logout");
-// logOutBtn.addEventListener("click", (e) => {
-//     e.preventDefault;
-//     signOut(auth);
-//     alert("登出成功!");
-// })
+
