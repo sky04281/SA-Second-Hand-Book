4084fed42dd7c1471bc05b31ed09fee51c9a4158
sky04281 <sky04282@gmail.com>
2023-04-24 12:04:46 +0800

    Update viewproduct.js

--- scripts/viewproduct.js
+++ scripts/viewproduct.js
@@ -4,26 +4,30 @@ import { collection, query, where, getDocs, doc, deleteDoc} from "https://www.gs
 
 
 
-const user = onAuthStateChanged(auth, async (user) => {
+onAuthStateChanged(auth, async (user) => {
     if(user){
         const ref = collection(db, "Product");
         const q = query(ref, where("sellerId", "==", user.uid));
         const querySnapshot = await getDocs(q);
         const view = document.getElementById("viewbook");
-        querySnapshot.forEach((docs) => {
-            var refDoc = doc(db ,"Product", docs.id);
+        
+        
+        querySnapshot.forEach( (docs) => {
+            
+
             view.innerHTML = view.innerHTML +
             "<tr>" +
                 "<td class='align-middle'><img src='' alt='' style='width: 50px;'>" + docs.data().book + "</td>" +
                 "<td class='align-middle'>"+
                     "<button class='btn btn-sm'><a href='editbook.html'><i class='fa fa-pen'></i></a></button>"+
-                    "<button class='btn btn-sm' id='"+ docs.id + "' onclick='deleteDoc("+ refDoc +")' "+ "><i class='fa fa-trash'></i></button>"+
+                    "<button class='btn btn-sm' id='"+ docs.id + "'><i class='fa fa-trash'></i></button>"+
                 "</td>"+
-            "</tr>";  
+            "</tr>";
         });
     }else{
         alert("請先登入!");
         location.href = "./index.html";
     }
 });
+
 
