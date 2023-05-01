2eae90b244a25c6781a05b137d5ea2ac00b36cf8
sky04281 <sky04282@gmail.com>
2023-05-01 15:24:10 +0800

    Squashed commit of the following:
    commit 5b069c5e2cc6def3863a4c5b3c62a7ea44e504e2
    Author: sky04281 <sky04282@gmail.com>
    Date:   Mon May 1 15:21:29 2023 +0800
    
        導覽列模組化
    
    commit faf455d1737b0e9aad7b099d3e51cb525efdf56b
    Author: sky04281 <sky04282@gmail.com>
    Date:   Sun Apr 30 19:33:34 2023 +0800
    
        OK
    
    commit 65f0c3b5b4216293ad1eecb60b650ca25f8d11ca
    Author: snoopirn <annica920831@gmail.com>
    Date:   Sun Apr 30 19:28:10 2023 +0800
    
        Update shop.html
    
    commit 540768e04766ec6b8e3ce6ea1c35700563721428
    Author: sky04281 <sky04282@gmail.com>
    Date:   Sun Apr 30 17:39:57 2023 +0800
    
        看得到了
    
    commit 1621dd94993e2fbd26b0fea1b28f072c13016b78
    Author: snoopirn <annica920831@gmail.com>
    Date:   Sun Apr 30 17:03:01 2023 +0800
    
        Update buyingbook.html
    
    commit 1d270c1ae03b5a0c46256641200609789e4034ff
    Author: snoopirn <annica920831@gmail.com>
    Date:   Sun Apr 30 16:55:18 2023 +0800
    
        Update shop.html

---
+++ scripts/shop.js
@@ -0,0 +1,52 @@
+import { auth, db } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { collection, query, where, getDocs, doc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+
+const ref = collection(db, "Product");
+// const q = query(ref, where("sellerId", "==", user.uid));
+const querySnapshot = await getDocs(ref);
+const view = document.querySelector('.viewbook');
+
+//書籍
+querySnapshot.forEach( (docs) => {
+    console.log(docs.data().book);
+    view.innerHTML = view.innerHTML +
+        "<div class='col-lg-4 col-md-6 col-sm-12 pb-1'>"+
+            "<div class='card product-item border-0 mb-4'>"+
+                "<div class='card-header product-img position-relative overflow-hidden bg-transparent border p-0'>"+
+                    "<a href='buyingbook.html' title=''><img src='img/product-2.jpg' class='img-fluid w-100' alt=''></a>"+
+                "</div>"+
+                "<div class='card-body border-left border-right text-center p-0 pt-4 pb-3'>"+
+                    "<a href='buyingbook.html' class='active'><h6 class='text-truncate mb-3'>"+ docs.data().book +"</h6></a>"+
+                    "<div class='d-flex justify-content-center'>"+
+                        "<h6>" + "NT$" + docs.data().price + "</h6>"+
+                    "</div>"+
+                "</div>"+
+            "</div>"+
+        "</div>";
+    
+});
+
+//分頁按鈕
+view.innerHTML = view.innerHTML +
+    "<div class='col-12 pb-1'>"+
+        "<nav aria-label='Page navigation'>"+
+            "<ul class='pagination justify-content-center mb-3'>"+
+                "<li class='page-item disabled'>"+
+                    "<a class='page-link' href='#' aria-label='Previous'>"+
+                        "<span aria-hidden='true'>&laquo;</span>"+
+                        "<span class='sr-only'>Previous</span>"+
+                    "</a>"+
+                "</li>"+
+                "<li class='page-item active'><a class='page-link' href='#'>1</a></li>"+
+                "<li class='page-item'><a class='page-link' href='#'>2</a></li>"+
+                "<li class='page-item'><a class='page-link' href='#'>3</a></li>"+
+                "<li class='page-item'>"+
+                    "<a class='page-link' href='#' aria-label='Next'>"+
+                        "<span aria-hidden='true'>&raquo;</span>"+
+                        "<span class='sr-only'>Next</span>"+
+                    "</a>"+
+                "</li>"+
+            "</ul>"+
+        "</nav>"+
+    "</div>";
