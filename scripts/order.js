f177ecaf2256bc96de1b5c835aa10fb515e287b5
lfishuaj13th <a0970292525@gmail.com>
2023-05-04 16:17:21 +0800

    訂單

---
+++ scripts/order.js
@@ -0,0 +1,10 @@
+import { auth, db } from "../scripts/firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+
+const delivery = document.getElementById("delivery");
+const address = document.getElementById("address");
+const payment = document.getElementById("payment");
+const others = document.getElementById("others");
+const btn = document.getElementById("btn-order");
+var date = new Date(); 
