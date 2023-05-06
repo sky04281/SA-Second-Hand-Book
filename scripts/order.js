38e761183963ae392256079ecacf03fc5b5d003c
lfishuaj13th <a0970292525@gmail.com>
2023-05-06 20:50:50 +0800

    上傳訂單資料但是還沒抓productId

--- scripts/order.js
+++ scripts/order.js
@@ -1,6 +1,6 @@
 import { auth, db } from "../scripts/firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import { doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
 
 const delivery = document.getElementById("delivery");
 const address = document.getElementById("address");
@@ -9,15 +9,16 @@ const others = document.getElementById("others");
 const btn = document.getElementById("btn-order");
 var date = new Date(); 
 
-const colRef = document(db, "Product", "ayR4rRGvunlDOkjYA1dC");
+const colRef = doc(db, "Product", "ayR4rRGvunlDOkjYA1dC");
 
 onAuthStateChanged(auth, (user) =>{
     if(user){
         console.log(user);
         btn.addEventListener("click", (e) => {
             e.preventDefault();
-            addDoc(colRef, {
-                order: [delivery.value, address.value, payment.value, others.value],
+            updateDoc(colRef, {
+                buyerId: user.uid,
+                order: [delivery.value, address.value, payment.value, others.value]
             })
             .then(() => {
                 alert("訂單已傳送給賣家!")
