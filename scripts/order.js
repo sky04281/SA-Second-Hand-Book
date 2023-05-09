8a6cf6d05b1ac300a399891957105950d109b7ed
lfishuaj13th <a0970292525@gmail.com>
2023-05-09 13:59:35 +0800

    阿內喔

--- scripts/order.js
+++ scripts/order.js
@@ -18,8 +18,8 @@ onAuthStateChanged(auth, (user) =>{
             e.preventDefault();
             updateDoc(colRef, {
                 buyerId: user.uid,
-                order: [delivery.value, address.value, payment.value, others.value, "待賣家確認"], 
-                ordering: true
+                order: [delivery.value, address.value, payment.value, others.value, true], 
+                ordering: "待賣家確認"
             })
             .then(() => {
                 alert("訂單已傳送給賣家!")
--- scripts/sellernotify.js
+++ scripts/sellernotify.js
@@ -8,7 +8,7 @@ onAuthStateChanged(auth, async (user) => {
     if(user){
         // 用 sellerId 從資料庫抓出使用者上架的書
         const ref = collection(db, "Product");
-        const q = query(ref, where("sellerId", "==", user.uid), where("ordering", "==", true));
+        const q = query(ref, where("sellerId", "==", user.uid), where("order", "array-contains", true));
         const p = query(ref);
         const querySnapshot = await getDocs(q);
         const view = document.getElementById("sellernotify");
@@ -18,7 +18,12 @@ onAuthStateChanged(auth, async (user) => {
             view.innerHTML = view.innerHTML +
             "<tr>" +
                 "<td class='align-middle'><img src='' alt='' style='width: 50px;'>" + docs.data().book +"</td>" +
-                "<td class='align-middle'>"+ docs.data().order[4]+ "</td>" +
+                "<td class='align-middle text-left'>寄送方式: " + docs.data().order[0] +
+                    "<br>寄送地址: " + docs.data().order[1] +
+                    "<br>付款方式: " + docs.data().order[2] +
+                    "<br>備註: " + docs.data().order[3] +
+                "</td>" +
+                "<td class='align-middle'>"+ docs.data().ordering+ "</td>" +
                 "<td class='align-middle'>"+
                     "<button class='btn btn-sm btn-check' id='" + docs.id +"'><i class='fas fa-check'>確認訂單</i></button>"+
                     "<button class='btn btn-sm btn-delete' id='"+ docs.id + "'><i class='fas fa-times'>取消訂單</i></button>"+
@@ -26,7 +31,7 @@ onAuthStateChanged(auth, async (user) => {
             "</tr>";
         });
 
-        if(order[4]=="待確認"){
+
             var btn1 = document.querySelectorAll('.btn-check');
             btn1.forEach((b) => {
                 b.addEventListener('click', (e) => {
@@ -58,7 +63,7 @@ onAuthStateChanged(auth, async (user) => {
                     });
                 });
             });
-        }
+        
 
     }else{
         alert("請先登入!");
