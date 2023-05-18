bd28eedb3371de21ed32af78e461d8217bc6c622
iris20021018 <130850862+iris20021018@users.noreply.github.com>
2023-05-18 17:00:59 +0800

    Update buyernotify.js

--- scripts/buyernotify.js
+++ scripts/buyernotify.js
@@ -9,6 +9,7 @@ onAuthStateChanged(auth, async (user) => {
         // 用 sellerId 從資料庫抓出使用者上架的書
         const ref = collection(db, "Product");
         const q = query(ref, where("buyerId", "==", user.uid), where("order", "array-contains", true), where("ordering", "==", "待賣家確認"));
+        const n = query(ref, where("buyerId", "==", user.uid), where("order", "array-contains", false), where("ordering", "==", "取消訂單"));
         const p = query(ref, where("buyerId", "==", user.uid), where("order", "array-contains", true), where("ordering", "==", "賣家接收訂單，交易成立"));
         const r = query(ref, where("buyerId", "==", user.uid), where("order", "array-contains", true), where("ordering", "==", "賣家已出貨，待買家收取並完成訂單"));
         const s = query(ref, where("buyerId", "==", user.uid), where("order", "array-contains", true), where("ordering", "==", "買家已完成訂單"));
@@ -16,6 +17,7 @@ onAuthStateChanged(auth, async (user) => {
         const querySnapshot_p = await getDocs(p);
         const querySnapshot_r = await getDocs(r);
         const querySnapshot_s = await getDocs(s);
+        const querySnapshot_n = await getDocs(n);
         const view = document.getElementById("buyernotify");
 
         // 把書本列出來
@@ -29,7 +31,7 @@ onAuthStateChanged(auth, async (user) => {
                     "<br>備註: " + docs.data().order[3] +
                 "</td>" +
                 "<td class='align-middle'>"+ docs.data().ordering+ "</td>" +
-                "<td class='align-middle'><i class='fas fa-times'></i></td>"+
+                "<td class='align-middle'><button class='btn btn-sm btn-cancel' id='" + docs.id +"'><i class='fas fa-poo'>取消訂單</i></button>"+"</td>"+
             "</tr><br>";
         });
 
@@ -76,6 +78,19 @@ onAuthStateChanged(auth, async (user) => {
                 "<td class='align-middle'>" + 
                     "<button class='btn btn-sm btn-comment' id='" + docs.id +"' href='comment.html'><i class='fas fa-arrow-right'>前往評價</i></button>" + 
                 "</td>"+
+            "</tr><br>";
+        });
+        querySnapshot_n.forEach( (docs) => {
+            view.innerHTML = view.innerHTML +
+            "<tr><td colspan='4'>已取消訂單</td></tr><tr>" +
+                "<td class='align-middle'><img src='' alt='' style='width: 50px;'>" + docs.data().book +"</td>" +
+                "<td class='align-middle text-left'>寄送方式: " + docs.data().order[0] +
+                    "<br>寄送地址: " + docs.data().order[1] +
+                    "<br>付款方式: " + docs.data().order[2] +
+                    "<br>備註: " + docs.data().order[3] +
+                "</td>" +
+                "<td class='align-middle'>"+ docs.data().ordering+ "</td>" +
+                "<td class='align-middle'> 取消成功</td>"+
             "</tr><br>";
         });
 
