65ca8abfa35898bf218be79d4e7bf115987fa15f
lfishuaj13th <a0970292525@gmail.com>
2023-06-03 16:07:47 +0800

    都是求書的東西

--- scripts/wanted.js
+++ scripts/wanted.js
@@ -1,6 +1,7 @@
-import { auth, db } from "../scripts/firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
-import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import { auth, db, storage } from "../scripts/firebase.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { collection, addDoc, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import { ref, uploadBytes } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-storage.js";
 
 //接值
 const book = document.getElementById("book");
@@ -11,39 +12,114 @@ const cate = document.getElementById("cate");
 const info = document.getElementById("info");
 const btn = document.getElementById("btn-wanted");
 var date = new Date(); 
+let imgSrc = "Product/";
 
 const colRef = collection(db, "Wanted");
 
 //如果登入再上架
-onAuthStateChanged(auth, (user) =>{
+onAuthStateChanged(auth, async(user) =>{
     if(user){
         console.log(user);
         //已通過身分驗證
         if(user.emailVerified == true){
+            const userRef = doc(db, "Account", user.uid);
+            const userSnap = await getDoc(userRef);
+            const data = userSnap.data();
+
+            imgSrc += (user.uid + "/");
+
             btn.addEventListener("click", (e) => {
                 e.preventDefault();
+                
+                //存圖片
+                if (imgFile) {
+                    const imgRef = ref(storage, imgSrc);
+                    uploadBytes(imgRef, imgFile);
+                }else{
+                    imgSrc = "Product/NotFound.jpg";
+                }
+                
+                //寄送方式
+                let delchecked = document.querySelectorAll('input[name="checkbox"]:checked');
+                let deloutput = [];
+                delchecked.forEach((checkbox) => {
+                    deloutput.push(checkbox.value);
+                });
+
+                while(deloutput.length<4){
+                    deloutput.push("");
+                }
+
+                //付款方式
+                let paychecked = document.querySelectorAll('input[name="checkbox1"]:checked');
+                let payoutput = [];
+                paychecked.forEach((checkbox) => {
+                    payoutput.push(checkbox.value);
+                });
+
+                while(payoutput.length<3){
+                    payoutput.push("");
+                }
+
                 addDoc(colRef, {
                     book: book.value,
                     author: author.value,
                     publish: publish.value,
-                    publish:"",
                     isbn: isbn.value,
-                    //cate: cate.value,
+                    price: parseInt(price.value),
+                    category: [data.area, data.school, data.college, data.department, cate.value],
+                    // [0: 地區, 1: 學校, 2: 學院, 3: 科系, 4: 科目]
                     category: [],
                     info: info.value,
                     sellerId: "",
                     buyerId: user.uid,
-                    date: date.toLocaleDateString(),
-                    deadline: "",
+                    date: date,
                     order: [], 
                     ordering: "",
                     setuptime: "",
                     deadline: "",
                     imgsrc: imgSrc,
-                    //delivery: deloutput,
-                    //pay: payoutput
+                    delivery: deloutput,
+                    pay: payoutput
                 })
-                .then(() => {
+                .then(async () => {
+                    const totalRef = doc(db, "Account", "Account_Total");
+                        const totalSnap = await getDoc(totalRef);
+                        let totalArea = totalSnap.data().totalArea;
+                        
+                        //新增類別到該科系
+                        totalArea.forEach((a)=>{
+                            //找到相同的地區
+                            if(a.area == data.area){
+                                let totalSchool = a.totalSchool;
+                                totalSchool.forEach((s)=>{
+                                    //找到相同的學校
+                                    if(s.school == data.school){
+                                        let totalCollege = s.totalCollege;
+                                        totalCollege.forEach((c)=>{
+                                            //找到相同的學院
+                                            if(c.college == data.college){
+                                                let totalDepartment = c.totalDepartment;
+                                                totalDepartment.forEach(async (d)=>{
+                                                    //找到相同的科系
+                                                    if (d.department == data.department) {
+                                                        let totalCate = d.totalCate;
+                                                        //假如類別不存在 就加入到該科系底下
+                                                        if(totalCate.includes(cate.value) == false){
+                                                            totalCate.push(cate.value);
+                                                            await updateDoc(totalRef, {
+                                                                totalArea: totalArea
+                                                            });
+                                                        }
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+
                     alert("新增成功!");
                     location.href = "./index.html";
                 });
@@ -60,3 +136,60 @@ onAuthStateChanged(auth, (user) =>{
         location.href = "./login.html";
     }   
 });
+
+//上傳照片
+const upload = document.getElementById('upload');
+const img = document.getElementById('img-img');
+const imginput = document.getElementById('img-input');
+const imgbtn = document.getElementById("img-btn");
+let imgFile;
+
+upload.addEventListener("dragenter", dragenter, false);
+upload.addEventListener("dragover", dragover, false);
+upload.addEventListener("drop", drop, false);
+imgbtn.addEventListener("click", click, false);
+img.addEventListener("click", click, false);
+imginput.addEventListener("change", 
+    function onchange(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        handleFiles(this.files);
+    }, false);
+
+function dragenter(e){
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+function dragover(e){
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+function drop(e){
+    e.stopPropagation();
+    e.preventDefault();
+
+    const dt = e.dataTransfer;
+    const files = dt.files;
+    handleFiles(files);
+}
+
+function click(e){
+    e.stopPropagation();
+    e.preventDefault();
+    imginput.click();
+}
+
+function handleFiles(files){
+    imgFile = files[0];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        img.src = e.target.result;
+    }
+    reader.readAsDataURL(imgFile);
+    img.style.display = 'block';
+    imgbtn.style.display = 'none';
+    upload.style.padding = '5%';
+    imgSrc += imgFile.name;
+}
---
+++ scripts/wantedshop.js
@@ -0,0 +1,275 @@
+import { auth, db, storage } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+import { collection, query, where, and, getDocs, getDoc, doc, orderBy, startAt, endAt } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+import { ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-storage.js";
+
+const booksRef = collection(db, "Wanted");
+const view = document.querySelector('.viewbook');
+const dropdown = document.querySelector('.dropdown-search');
+const btn = document.querySelector('.btn-search');
+const form = document.querySelector('.from-search');
+
+let search = document.getElementById('input-search');
+let cateKey = "";
+let cateValue = "";
+let queryArr = [];
+let q, querySnapshot;
+
+console.log(sessionStorage);
+
+//分類選單-渲染
+{
+dropdown.innerHTML = 
+    "<div class='nav-item dropdown'>"+
+        "<a href='#' class='nav-link' data-toggle='dropdown' name='area'>地區 <i"+
+                "class='fa fa-angle-down float-right mt-1'></i></a>"+
+        "<div class='dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0'>"+
+            "<a href='' class='dropdown-item'>北北基</a>"+
+            "<a href='' class='dropdown-item'>桃竹苗</a>"+
+            "<a href='' class='dropdown-item'>中彰投</a>"+
+            "<a href='' class='dropdown-item'>雲嘉南</a>"+
+        "</div>"+
+    "</div>"+
+    "<div class='nav-item dropdown'>"+
+        "<a href='#' class='nav-link' data-toggle='dropdown' name='school'>學校 <i"+
+                "class='fa fa-angle-down float-right mt-1'></i></a>"+
+        "<div class='dropdown-school dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0'>"+
+        "</div>"+
+    "</div>"+
+    "<div class='nav-item dropdown'>"+
+        "<a href='#' class='nav-link' data-toggle='dropdown' name='college'>學院 <i"+
+                "class='fa fa-angle-down float-right mt-1'></i></a>"+
+        "<div class='dropdown-college dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0'>"+
+        "</div>"+
+    "</div>"+
+    "<div class='nav-item dropdown'>"+
+        "<a href='#' class='nav-link' data-toggle='dropdown' name='department'>科系 <i"+
+                "class='fa fa-angle-down float-right mt-1'></i></a>"+
+        "<div class='dropdown-department dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0'>"+
+        "</div>"+
+    "</div>"+
+    "<div class='nav-item dropdown'>"+
+        "<a href='#' class='nav-link' data-toggle='dropdown' name='cate'>類別 <i"+
+                "class='fa fa-angle-down float-right mt-1'></i></a>"+
+        "<div class='dropdown-cate dropdown-menu position-absolute bg-secondary border-0 rounded-0 w-100 m-0'>"+
+        "</div>"+
+    "</div>";
+}
+
+//分類選單-功能
+// {
+// const totalRef = doc(db, "Account", "Account_Total");
+// const totalSnap = await getDoc(totalRef);
+// const tschool = totalSnap.data().tschool; 
+// const tcollege = totalSnap.data().tcollege; 
+// const tdepartment = totalSnap.data().tdepartment;
+// const tcate = totalSnap.data().tcate;
+
+// //抓取已有的學校、學院、科系，並且渲染出來
+// tschool.forEach((s) =>{
+//     document.querySelector('.dropdown-school').innerHTML += 
+//         ("<a href='' class='dropdown-item'>" + s + "</a>");
+// });
+// tcollege.forEach((c) =>{
+//     document.querySelector('.dropdown-college').innerHTML += 
+//         ("<a href='' class='dropdown-item'>" + c + "</a>");
+// });
+// tdepartment.forEach((d) =>{
+//     document.querySelector('.dropdown-department').innerHTML += 
+//         ("<a href='' class='dropdown-item'>" + d + "</a>");
+// });
+// tcate.forEach((c) =>{
+//     document.querySelector('.dropdown-cate').innerHTML +=
+//         ("<a href='' class='dropdown-item'>" + c + "</a>");
+// });
+
+// //點擊分類按鈕
+// const navLink = document.querySelectorAll('.nav-link');
+// navLink.forEach((link) => {
+//     link.addEventListener("click", () =>{
+//         cateKey = link.textContent;
+//         console.log(cateKey);
+//     });
+// });    
+// const dropdownItem = document.querySelectorAll('.dropdown-item');
+// dropdownItem.forEach((item) => {
+//     item.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         cateValue = item.textContent;
+//         console.log(cateValue);
+//         myQuery();
+//     });
+// });
+// }
+
+//搜尋欄
+{
+btn.addEventListener("click", async (e) => {
+    e.preventDefault();
+    myQuery();
+});
+
+form.addEventListener("submit", async (e)=>{
+    e.preventDefault();
+    myQuery();
+});
+}
+
+myQuery();
+
+//書籍渲染
+function show(){
+    //排序按鈕
+    view.innerHTML = 
+        "<div class='col-12 pb-1'>" +
+            "<div class='d-flex align-items-center justify-content-between mb-4'>" +
+                "<form action=''>" +
+                    "<a href='shop.html'>全部商品</a> >" + cateKey + ">" + cateValue +
+                "</form>" +
+                "<div class='dropdown ml-4'>" +
+                    "<button class='btn border dropdown-toggle' type='button' id='triggerId'"  +
+                        "data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>" +
+                                "排序" +
+                    "</button>" +
+                "<div class='dropdown-menu dropdown-menu-right' aria-labelledby='triggerId'>" +
+                        // "<a class='dropdown-item' href='#'>上架日期由近至遠</a>" +
+                        // "<a class='dropdown-item' href='#'>上架日期由遠至近</a>" +
+                        "<a class='sort-price dropdown-item' href='#' id='-1'>價格由高至低</a>" +
+                        "<a class='sort-price dropdown-item' href='#' id='1'> 價格由低至高</a>" +
+                    "</div>" +
+                "</div>" +
+            "</div>" +
+        "</div>"+
+        "<div class='view-test row'></div>";
+    
+    const viewTest = document.querySelector('.view-test');
+    //書籍
+    queryArr.forEach((docs) => {
+        viewTest.innerHTML = viewTest.innerHTML +
+            "<div class='col-lg-4 col-md-6 col-sm-12 pb-1'>"+
+                "<div class='card product-item border-0 mb-4'>"+
+                    "<div class='card-header product-img position-relative overflow-hidden bg-transparent border p-0'>"+
+                        "<a href='buyingbook.html?bookId=" + docs.id + "' title=''><img id='"+ docs.id +"' src='' class='img-fluid w-100' alt=''></a>"+
+                    "</div>"+
+                    "<div class='card-body border-left border-right text-center p-0 pt-4 pb-3'>"+
+                        "<a href='buyingbook.html?bookId=" + docs.id + "' class='active'><h6 class='text-truncate mb-3'>"+ docs.data.book +"</h6></a>"+
+                        "<div class='d-flex justify-content-center'>"+
+                            "<h6>" + "NT$" + docs.data.price + "</h6>"+
+                        "</div>"+
+                    "</div>"+
+                "</div>"+
+            "</div>";
+
+        const imgRef = ref(storage, docs.data.imgsrc);
+        getDownloadURL(imgRef).then((url) => {
+            var img = document.getElementById(docs.id);
+            img.setAttribute('src', url);
+        });
+    });
+
+    //分頁按鈕
+    view.innerHTML = view.innerHTML +
+    "<div class='col-12 pb-1'>"+
+        "<nav aria-label='Page navigation'>"+
+            "<ul class='pagination justify-content-center mb-3'>"+
+                "<li class='page-item disabled'>"+
+                    "<a class='page-link' href='#' aria-label='Previous'>"+
+                        "<span aria-hidden='true'>&laquo;</span>"+
+                        "<span class='sr-only'>Previous</span>"+
+                    "</a>"+
+                "</li>"+
+                "<li class='page-item active'><a class='page-link' href='#'>1</a></li>"+
+                "<li class='page-item'><a class='page-link' href='#'>2</a></li>"+
+                "<li class='page-item'><a class='page-link' href='#'>3</a></li>"+
+                "<li class='page-item'>"+
+                    "<a class='page-link' href='#' aria-label='Next'>"+
+                        "<span aria-hidden='true'>&raquo;</span>"+
+                        "<span class='sr-only'>Next</span>"+
+                    "</a>"+
+                "</li>"+
+            "</ul>"+
+        "</nav>"+
+    "</div>";
+
+    search.value = "";
+
+    const sortPrice = document.querySelectorAll('.sort-price');
+    sortPrice.forEach((sp)=>{
+        sp.addEventListener('click', (e)=>{
+            e.preventDefault();
+            arrSort(queryArr, "price", parseInt(sp.id));
+            show();
+        });
+    });
+}
+
+
+
+//查詢功能
+async function myQuery(){
+    //有選分類
+    if((search.value == "") && (cateKey != "")){
+        q = query(booksRef, where("category", "array-contains", cateValue));
+        querySnapshot = await getDocs(q);
+    }
+    else if((search.value != "") && (cateKey != "")){
+        q = query(booksRef, where("category", "array-contains", cateValue), orderBy("book"), startAt(search.value), endAt(search.value + '\uf8ff'));
+        querySnapshot = await getDocs(q);
+    }
+    //沒選分類
+    else if (search.value == "") {
+        querySnapshot = await getDocs(booksRef);
+    }
+    else{
+        q = query(booksRef, orderBy("book"), startAt(search.value), endAt(search.value + '\uf8ff'));
+        querySnapshot = await getDocs(q);
+    }
+
+    //放到自訂的陣列裡處理排序
+    queryArr = [];
+    querySnapshot.forEach((docs) => {
+        queryArr.push({
+            id: docs.id,
+            data: docs.data()
+        });
+    });
+
+    //預設價格小到大
+    arrSort(queryArr, "price");
+    show();
+    search.value = "";
+}
+
+//排序
+function arrSort(arr = [], key = "price", choose = 1){
+    var temp;
+    switch (key) {
+        case "price":
+            if (choose == -1) {
+                for (let i = 0; i < arr.length; i++) {
+                    for(let j = 0; j < arr.length -1; j++){
+                        if (arr[j].data.price < arr[j + 1].data.price) {
+                            temp = arr[j + 1];
+                            arr[j + 1] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+            }else{
+                for (let i = 0; i < arr.length; i++) {
+                    for(let j = 0; j < arr.length -1; j++){
+                        if (arr[j].data.price > arr[j + 1].data.price) {
+                            temp = arr[j + 1];
+                            arr[j + 1] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+            }
+            
+            break;
+        case "date":
+            break;
+    }
+    
+}
